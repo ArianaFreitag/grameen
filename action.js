f08161dd9c4886dbc4056d48abc251c1b7fccaca
Ariana Freitag <freitagariana@gmail.com>
2020-02-06 13:38:12 -0500

    changes

--- action.js
+++ action.js
@@ -1,12 +1,22 @@
-function PlaySound(soundobj) {
+function PlaySound(soundobj, num) {
     var thissound=document.getElementById(soundobj);
     thissound.play();
+    var img=document.getElementById("gif");
+    img.src = "beer-01.png";
+
+    var text = document.getElementById("text_"+num);
+    text.style.display="block";
 }
 
-function StopSound(soundobj) {
+function StopSound(soundobj, num) {
     var thissound=document.getElementById(soundobj);
     thissound.pause();
     thissound.currentTime = 0;
+    var img=document.getElementById("gif");
+    img.src = "test.gif";
+
+    var text = document.getElementById("text_"+num);
+    text.style.display="none";
 }
 
 function changeImg() {
